dc0c7c124da914ffbefaebdbfe4f0fa4c34974aa
Ludwig Bethnicer Cagas-Napigkit <napigkitludwigbethnicer@gmail.com>
2022-04-05 18:10:03 +0800

    new repo

---
+++ content/theme/kjjsclosets/template-part/aboutus.php
@@ -0,0 +1,21 @@
+	<div class="<?php echo $contentwidth; ?>">
+		<div class="pt-5 pb-5">
+			<div class="row">
+				<div class="col-sm-8">
+					<h2>About KJJ's Closets</h2>
+					<br>
+					<h5>The KJJs website is a website that bring the customers closer to thier dream fashion, and having a lot of good quality products that enables the customer to search, find, order and pay for the products and gives information and services that they need.</h5>
+					<br>
+					<p>In this modern way of shopping KJJ's gives a convenient way of shopping by just one click, and easily search any item you love by browsing our system. <b>So what are you waiting for? Add to cart now!!</b></p>
+					<br>
+					<a href="<?php echo $domainhome; ?>routes/contactus">
+						<button class="<?php echo 'btn '.$buttonsize; ?>" style="<?php echo 'background-color: '.$primarycolor.';'; ?>">Get in Touch</button>
+					</a>
+				</div>
+				<div class="col-sm-4 text-center">
+					<img class="frontpageimglogo slideanim" src="<?php echo $domainhome.'content/theme/'.$themename.'/storage/img/'.$syslogo; ?>">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
